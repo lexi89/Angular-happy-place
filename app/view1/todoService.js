4a8ba94a076c11f714d2cc5f361bac077dcdeb3e
Alexey Yanovski <alexey.yanovski1@gmail.com>
2016-07-25 23:49:25 +0100

    auto-commit

--- app/view1/todoService.js
+++ app/view1/todoService.js
@@ -1,23 +1,27 @@
 // Services should have one responsibility. Typically fetching and managing data storage. If a factory begins to take on more than one responsibility, split off into another.
 
-angular
-  .module("todos")
-  .factory("todoservice", todoservice);
+(function(){
 
-  function todoservice() {
-    // put all variable and function declarations at the top for readability.
-    var service = {
-      hello: hello,
-      goodbye: goodbye
-    };
-    return service;
+  angular
+    .module("todos")
+    .factory("todoservice", todoservice);
 
+    function todoservice() {
+      // put all variable and function declarations at the top for readability.
+      var service = {
+        hello: hello,
+        goodbye: goodbye
+      };
+      return service;
 
-    // keep implementation details "below the fold"
-    function hello() {
-      console.log("hello");
+      // keep implementation details lower down.
+      function hello() {
+        console.log("hello from a service");
+      }
+      function goodbye() {
+        console.log("goodbye from a service");
+      }
     }
-    function goodbye() {
-      console.log("goodbye");
-    }
-  }
+
+
+})();
--- app/view1/todos.js
+++ app/view1/todos.js
@@ -1,20 +1,20 @@
 angular
-  .module("todos", ["ngRoute"])
-  .controller("todoCtrl", TodoController)
-  .config(config);
+    .module("todos", ["ngRoute"])
+    .controller("todoCtrl", TodoController)
+    .config(config);
 
-TodoController.$inject = ["todoservice"];
+  TodoController.$inject = ["todoservice"];
 
-function TodoController(){
-  this.example = "Buy milk";
-  this.hello = todoservice().hello;
-  this.goodbye = todoservice().goodbye;
-}
+  function TodoController(todoservice){
+    this.example = "Buy milk";
+    this.hello = todoservice.hello;
+    this.goodbye = todoservice.goodbye;
+  }
 
-function config($routeProvider){
-  $routeProvider.when("/view1", {
-    templateUrl: "/view1/todoView.html",
-    controller: "todoCtrl",
-    controllerAs: "todos"
-  });
-}
+  function config($routeProvider){
+    $routeProvider.when("/view1", {
+      templateUrl: "/view1/todoView.html",
+      controller: "todoCtrl",
+      controllerAs: "todos"
+    });
+  }
