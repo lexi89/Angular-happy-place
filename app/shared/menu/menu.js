a596a7b0f37a5d06de5affe71a6190f41dd903dc
Alexey Yanovski <alexey.yanovski1@gmail.com>
2016-08-15 11:39:05 +0100

    tidy up

--- app/shared/menu/menu.js
+++
@@ -1,6 +0,0 @@
-angular.module("app")
-.directive("menu", function(){
-  return {
-    templateUrl: "/shared/menu/menu.html"
-  };
-});
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-var apiController = require("./controllers/apiController");
 var sessions = require("client-sessions");
 var bodyParser = require("body-parser");
 
@@ -15,32 +14,9 @@ app.use("/", express.static(__dirname +"/app"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(sessions({
-  cookieName: "session",
-  secret: "iminyourcookies",
-  duration: 24*60*60*1000,
-  activeDuration: 1000 * 60 * 5
-}));
-
-app.use(function(req, res, next){
-  if (!req.session.questions){
-    req.session.questions = [];
-  }
-  next();
-});
-
-app.get("/dashboard", function(req, res){
-  res.render("dashboard/dashboard.ejs");
-});
-
 app.get("/", function (req, res) {
-  res.render("homepage.ejs");
+  res.render("index.ejs");
 });
-
-// setup APIs
-apiController(app);
-// sessionController(app);
-
 
 app.listen(port, function(){
   console.log("Node server is running on port:" + port);
